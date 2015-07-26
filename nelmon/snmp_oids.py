889efdb601c1175c641dcc82afcdd9b62528db90
ogenstad <patrick@ogenstad.com>
2015-07-26 09:53:26 +0200

    Changed oid import

--- nelmon/snmp_oids.py
+++ nelmon/snmp_oids.py
@@ -1,3 +1,7 @@
 from nelsnmp.cisco_oids import CiscoOids
 from nelsnmp.oids import GeneralOids
 from nelsnmp.synology_oids import SynologyOids
+
+cisco_oids = CiscoOids()
+general_oids = GeneralOids()
+synology_oids = SynologyOids()
